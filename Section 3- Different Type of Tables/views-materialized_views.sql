7bd4f77b61eb94ac21c500a1e1c71c3b72d165da
Hua Wang <huawang@Huas-MacBook-Pro.local>
2023-02-13 16:50:35 -0800

    switch to secure materialized view

--- Section 3- Different Type of Tables/views-materialized_views.sql
+++ Section 3- Different Type of Tables/views-materialized_views.sql
@@ -1,23 +1,23 @@
 use role sysadmin ; 
 
 --- Change the warehouse name if need be ----
-use warehouse prod_xl; 
+use warehouse compute_wh; 
 use database ecommerce_db;
 use schema ecommerce_db.ecommerce_liv;
 
 -- View for Orders with "Urgent" Priority ---
 create or replace view urgent_priority_orders as 
-select * from "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS" where o_orderpriority='1-URGENT'
+select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS" where o_orderpriority='1-URGENT';
 
 
 -- Create a materialized view ---
-create or replace materialized view vw_aggregated_orders as 
+create or replace secure materialized view vw_aggregated_orders as 
 select 
     count(1) as total_orders, 
     O_ORDERSTATUS as order_status , 
     O_ORDERDATE as order_date
 from 
-    "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS"
+    "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS"
 where o_orderpriority='1-URGENT'
 group by 2,3;
 
