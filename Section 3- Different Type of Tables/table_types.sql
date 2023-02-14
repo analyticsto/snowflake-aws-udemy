9846a6a014fbaea08ba3c2c47a8dee8d373f3e16
Hua Wang <huawang@Huas-MacBook-Pro.local>
2023-02-13 16:25:41 -0800

    switch to raw snowflake sample order table

--- Section 3- Different Type of Tables/table_types.sql
+++ Section 3- Different Type of Tables/table_types.sql
@@ -7,28 +7,28 @@ use database ecommerce_db;
 use schema ecommerce_db.ecommerce_liv;
 
 -- Temporary TABLE ---
-create or replace temporary table orders_tmp as select * from "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS" limit 50;
+create or replace temporary table orders_tmp as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS"  limit 50;
 
 
 -- TRANSIENT TABLE ---
-create or replace transient table orders as select * from "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS" limit 50;
+create or replace transient table orders as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS"  limit 50;
 
 ---- Transient Schema ----
 create transient schema transient_schema;
 use schema transient_schema;
-create or replace table orders as select * from "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS" limit 50;
+create or replace table orders as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS"  limit 50;
 
 
 ---- Transient Database ----
 create transient database transient_db;
 create schema test_schema;
 use database transient_db;
-create or replace table orders as select * from "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS" limit 50;
+create or replace table orders as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS"  limit 50;
 
 
 
 --- Convert Permanent table to transient table ----- 
-create or replace table permanent_orders as select * from "ECOMMERCE_DB"."ECOMMERCE_LIV"."ORDERS" limit 50;
+create or replace table permanent_orders as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS" limit 50;
 
 create or replace transient table transient_orders as select * from permanent_orders limit 50;
 
